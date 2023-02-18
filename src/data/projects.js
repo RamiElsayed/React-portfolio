2e95a50df536e35714b33a02c4118106656acc90
Rami Elsayed <ramielsayed123@outlook.com>
2023-02-18 19:20:18 +0000

    edited images alt

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     image:
         "https://user-images.githubusercontent.com/81489678/204623840-f9165fb8-82f7-4698-817b-2991dc58bfac.png",
-    alt: "Contoso University",
+    alt: "Contoso University an imaginary name for the project",
     title: "Contoso University",
     description:
         "A sample application demonstrates the knowledge on using Entity Framework Core in ASP.NET Core MVC.",
@@ -11,7 +11,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/coding-forum/blob/main/public/images/coding-forum.jpg?raw=true",
-    alt: "Coding Forum",
+    alt: "screen shot of the Coding Forum application",
     title: "Coding Forum",
     description:
       "A forum to create threads and reviews for junior web developers.",
@@ -21,7 +21,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/Employee-Tracker/raw/main/src/images/employee-tracker.jpg",
-    alt: "Employee Tracker",
+    alt: "screen shot of the Employee Tracker",
     title: "Employee Tracker",
     description:
       "A simple app runs in the terminal allowing managers to control their employees rotas and positions.",
@@ -30,7 +30,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/film-go/raw/main/images/Project-screenshot.jpg",
-    alt: "Film-Go",
+    alt: "screen shot of the Film-Go",
     title: "Film-Go",
     description:
       "A front end project that allow users to search their favourite films with top youtube videos and data from IMDB.",
@@ -40,7 +40,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/password-generator/raw/main/assets/images/Password%20Generator.jpg",
-    alt: "Password Generator",
+    alt: "screen shot of the Password Generator",
     title: "Password Generator",
     description:
       "A front end project allowing users to generate random passwords to secure their accounts",
@@ -50,7 +50,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/React-portfolio/blob/main/src/assets/screenshot.png?raw=true",
-    alt: "React Portfolio",
+    alt: "screen shot of the React Portfolio",
     title: "React Portfolio",
     description: "A react page with links to some of my projects",
     repositoryLink: "https://github.com/RamiElsayed/React-portfolio.git",
@@ -59,7 +59,7 @@ const projects = [
   {
     image:
       "https://github.com/RamiElsayed/team-profile-generator/raw/main/images/team-profile-generator.jpg",
-    alt: "Team Profile Generator",
+    alt: "screen shot of the Team Profile Generator",
     title: "Team Profile Generator",
     description:
       "This is a project run in the terminal allowing user to create cards for their team members",
